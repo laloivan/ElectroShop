028c2b2653ac7c7087a3cb9f870ec8282d004cd5
Eduardo Contreras <programador3@etecno.com.mx>
2019-02-16 01:30:15 -0600

    nnn

---
+++ conexion.php
@@ -0,0 +1,7 @@
+<?php 
+$link= mysqli_connect("localhost","id8382908_ventacelulares","ventacelulares3436","id8382908_ventacelulares"); // conectando al servidor mysql
+if (!$link) // link es un nombre cualquiera de variable
+{
+echo "No se pudo conectar a la base de datos";
+}
+?>
